edc3663704a5363621323e173d164a1e03133d9e
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2023-01-03 01:06:14 +0900

    correction to the page_category

--- page_category.php
+++ page_category.php
@@ -2,7 +2,7 @@
 //DB接続
 include("db_connect.php");
 
-// カテゴリー別呼び出し
+//DBから商品データ呼び出し
 $category = $_GET['category'];
 if($category == "リスト"){
     $sql = "SELECT * FROM goods";
@@ -10,20 +10,16 @@ if($category == "リスト"){
     $sql = "SELECT * FROM goods WHERE category = '$category'";
 }
 $result_rows = $pdo->query($sql);
-?>
-
-
-<h2>個別データ取得</h2>
-
-<?php foreach ( $result_rows as $row ): ?>
-    <?= "goods_id: {$row['goods_id']} <br>" ?>
-    <?= "カテゴリー: {$row['category']} <br>"?>
-    <?= "商品名: {$row['goods_name']} <br>" ?>
-    <?= "金額: {$row['price']} <br" ?>
-    <div >
+//データ出力
+foreach ( $result_rows as $row ):
+    echo "カテゴリー: {$row['category']} <br>";
+    echo "商品名: {$row['goods_name']} <br>";
+    echo "金額: {$row['price']} <br>";
+    ?>
+    <div>
     <form method="post" action="index.php?page_select=page_cart">
         <input type="hidden" name="goods_id" value="<?= "{$row['goods_id']}" ?>">
-        <div>num <input type="number" name="num" value="1" min="1"></div>
+        <div>数量 <input type="number" name="num" value="1" min="1"></div>
         <div><input type="submit" value="カートに入れる"></div>
     </form>
     <br>
